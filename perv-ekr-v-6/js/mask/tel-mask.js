cf77a1d0d6d5e22df5506850fc4037dc3d0b3e0d
Aliaksei Karzhou <alexius90e@gmail.com>
2025-02-06 23:16:12 +0300

    fix: added int code placeholder on focus

--- perv-ekr-v-6/js/mask/tel-mask.js
+++ perv-ekr-v-6/js/mask/tel-mask.js
@@ -8,4 +8,17 @@ const phoneInputs = phoneInputClassNames
   .map((name) => document.querySelectorAll(`.${name}`))
   .reduce((acc, array) => [...acc, ...array], []);
 
-phoneInputs.forEach((inputElement) => IMask(inputElement, maskOptions));
+phoneInputs.forEach((inputElement) => {
+  const basePlaceholder = inputElement.getAttribute('placeholder');
+  const focusPlaceholder = '+7 (';
+
+  IMask(inputElement, maskOptions);
+
+  inputElement.addEventListener('focus', (event) => {
+    event.currentTarget.setAttribute('placeholder', focusPlaceholder);
+  });
+
+  inputElement.addEventListener('blur', (event) => {
+    event.currentTarget.setAttribute('placeholder', basePlaceholder);
+  });
+});
