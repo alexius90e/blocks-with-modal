a960fc71dc01911a6807036b3a8db2baf08155da
Aliaksei Karzhou <alexius90e@gmail.com>
2025-02-06 14:01:56 +0300

    feat: added imask

---
+++ perv-ekr-v-6/js/mask/tel-mask.js
@@ -0,0 +1,11 @@
+const phoneInputClassNames = ['imask-phone-input'];
+
+const maskOptions = {
+  mask: '{7} (000) 000-00-00[0]',
+};
+
+const phoneInputs = phoneInputClassNames
+  .map((name) => document.querySelectorAll(`.${name}`))
+  .reduce((acc, array) => [...acc, ...array], []);
+
+phoneInputs.forEach((inputElement) => IMask(inputElement, maskOptions));
